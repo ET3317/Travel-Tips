73558bef693e1d161482e7736c7ed64a34facd27
ETIENNE_JOUAULT <etienne.jouault@live.fr>
2023-07-19 15:45:07 +0200

    wip

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,39 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'style' => 'margin-top: 1vh;', // Ajoutez le style CSS personnalisé ici
+                ]
+            ])
+            //->add('roles')
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'style' => 'margin-top: 1vh;', // Ajoutez le style CSS personnalisé ici
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'style' => 'margin-top: 1vh;', // Ajoutez le style CSS personnalisé ici
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'style' => 'margin-top: 1vh;', // Ajoutez le style CSS personnalisé ici
+                ]
+            ])
         ;
     }
 
---
+++ src/Security/UserAuthenticator.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\{RedirectResponse, Request, Response};
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\{CsrfTokenBadge, RememberMeBadge, UserBadge};
+use Symfony\Component\Security\Http\Authenticator\Passport\{Credentials\PasswordCredentials, Passport};
+
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class UserAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+        $user = $token->getUser(); // Récupérer l'utilisateur connecté à partir du token
+
+        if ($user instanceof User) {
+            return new RedirectResponse($this->urlGenerator->generate('app_user_show', ['id' => $user->getId()]));
+        }
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
